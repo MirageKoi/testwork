eedf73a6411bf8a492267fcdc2fb695eeb8552f6
Denys <108667781+MirageKoi@users.noreply.github.com>
2023-10-18 09:47:45 +0300

    add Person model & tests

--- manager/team/models.py
+++ manager/team/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+
 class TeamModel(models.Model):
     title = models.CharField(max_length=128)
-    
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField()
--- manager/team/tests/test_models.py
+++ manager/team/tests/test_models.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
-from ..models import TeamModel
+from ..models import TeamModel, Person
 
-class TeamTest(TestCase):
-    """ Test module for Team """
+
+class TeamModelTest(TestCase):
+    """Test module for TeamModel"""
 
     def setUp(self) -> None:
         TeamModel.objects.create(title="First team")
@@ -10,3 +11,23 @@ class TeamTest(TestCase):
     def test_team_title(self):
         team_1 = TeamModel.objects.get(title="First team")
         self.assertEqual(team_1.title, "First team")
+
+
+class PersonModelTest(TestCase):
+    """Test module for PersonModel"""
+
+    def setUp(self) -> None:
+        Person.objects.create(
+            first_name="Bob", last_name="Cat", email="bobcat@gmail.com"
+        )
+        Person.objects.create(
+            first_name="Jack", last_name="Rich", email="jackrich@gmail.com"
+        )
+
+    def test_person_first_and_last_names(self):
+        person_1 = Person.objects.get(id=1)
+        person_2 = Person.objects.get(id=2)
+        self.assertEqual((person_1.first_name), "Bob")
+        self.assertEqual((person_1.last_name), "Cat")
+        self.assertEqual((person_2.first_name), "Jack")
+        self.assertNotEqual((person_2.last_name), "Wick")
