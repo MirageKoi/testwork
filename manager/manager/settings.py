c7e9e7a0105cfce824e6b2b7cdb419b2fcc5c53c
Denys <108667781+MirageKoi@users.noreply.github.com>
2023-10-19 15:55:23 +0300

    add API Docs; upd urls

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "team.apps.TeamConfig",
     # External apps
     "rest_framework",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,19 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# DRF Settings
+
+REST_FRAMEWORK = {
+    
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+# Spectacular Settings
+
+# SPECTACULAR_SETTINGS = {
+#     "TITLE": "Your Project API",
+#     "DESCRIPTION": "Your project description",
+#     "VERSION": "1.0.0",
+#     "SERVE_INCLUDE_SCHEMA": False,
+# }
--- manager/team/urls.py
+++ manager/team/urls.py
@@ -3,8 +3,8 @@ from .views import TeamAPIView, TeamDetailAPIView
 from .views import MemberAPIView, MemberDetailAPIView
 
 urlpatterns = [
-    path("team/", TeamAPIView.as_view(), name="list_team"),
-    path("team/<int:pk>", TeamDetailAPIView.as_view(), name="detail_team"),
+    path("teams/", TeamAPIView.as_view(), name="list_team"),
+    path("teams/<int:pk>", TeamDetailAPIView.as_view(), name="detail_team"),
     path("members/", MemberAPIView.as_view(), name="list_member"),
     path("members/<int:pk>", MemberDetailAPIView.as_view(), name="detail_member"),
 ]
